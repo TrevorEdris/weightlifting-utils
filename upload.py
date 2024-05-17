6e146437d50455ea5e854f88df8d5184542b8a2d
Trevor Edris <trevor.edris@gmail.com>
2024-05-17 13:55:35 -0400

    Avoid uploading duplicate rows

--- upload.py
+++ upload.py
@@ -21,6 +21,35 @@ CREDENTIALS_FILE = f"{CREDS_DIR}/creds.json"
 SPREADSHEET_ID = "1JJxZRA4OnsbzUOaL6aYvD5qSf1uq2uoO9Ew1c-TFIH4"
 SHEET_NAME = "Combined Data"
 
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+# Header row
+#    0        1         2              3             4              5
+# Person	Date	Workout Name	Duration	Exercise Name	Set Order
+# NOTE: Ensure your Google Sheets Date column is formatted to INCLUDE
+# NOTE: leading 0's on all componenets. Strong's CSV export includes them.
+# NOTE: Without this formatting, duplicate rows will not be detected, as
+# NOTE: this current implementation does a string comparison, not a
+# NOTE: logical date comparison.
+PERSON = 0
+DATE = 1
+WORKOUT_NAME = 2
+DURATION = 3
+EXERCISE_NAME = 4
+SET_ORDER = 5
+
+# For deduplication of data, we must figure out which rows of the incoming
+# data match a row in the existing data. To do this accurately, we need
+# to be able to uniquely identify a row.
+UNIQUE_ROW_KEY = [
+    PERSON,
+    DATE,
+    WORKOUT_NAME,
+    DURATION,
+    EXERCISE_NAME,
+    SET_ORDER,
+]
+
 
 def auth(credentials_file: str):
     creds = None
@@ -47,28 +76,87 @@ def auth(credentials_file: str):
     return creds
 
 
-def upload(
+def read_existing_data(
     creds,
-    person: str,
-    filename: str,
     spreadsheet_id: str,
     sheet_name: str,
-):
+) -> list[list]:
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values().get(spreadsheetId=spreadsheet_id, range=sheet_name).execute()
+        )
+        values = result.get("values", [])
+
+        if not values:
+            print("No data found")
+            return
+
+        print(f"Read {len(values)} rows from {spreadsheet_id} -> {sheet_name}")
+
+        return values
+    except HttpError as err:
+        print(err)
+
+
+def read_data_to_upload(
+    person: str,
+    filename: str,
+) -> list[list]:
     with open(filename, "r") as file:
         reader = csv.reader(file)
         raw = list(reader)[1:]  # Exclude header row
+
+    return [[person] + row for row in raw]
+
+
+def _rows_to_dict(data: list[list]) -> dict[str, list]:
+    result = {}
+    for row in data:
+        key_components = [f"{row[column_number]}" for column_number in UNIQUE_ROW_KEY]
+        key = "|".join(key_components)
+        result[key] = row
+    return result
+
+
+def deduplicate(
+    existing_data: list[list],
+    data_to_upload: list[list],
+) -> list[list]:
+    existing_data_lookup = _rows_to_dict(existing_data)
+    data_to_upload_lookup = _rows_to_dict(data_to_upload)
+    deduped = []
+    duplicate_count = 0
+    for k, v in data_to_upload_lookup.items():
+        if k in existing_data_lookup:
+            # Duplicate row, skip
+            duplicate_count += 1
+            continue
+        print(f"{k} not found in existing data")
+        deduped.append(v)
+    print(f"Removed {duplicate_count} duplicate row(s) from input data")
+    return deduped
+
+
+def upload(
+    creds,
+    spreadsheet_id: str,
+    sheet_name: str,
+    data: list[list],
+):
 
     # The exports from the Strong app obviously don't include a first name column
     # but distinguishing between personal exports is necessary when analyzing
     # a group of people's lifting data together, such as the use-case that
     # inspired this script.
-    rows = [[person] + row for row in raw]
-    print(f"Attempting to upload {len(rows)} rows to sheet")
+    print(f"Attempting to upload {len(data)} rows to sheet")
 
     try:
         service = build("sheets", "v4", credentials=creds)
 
-        value_range_body = {"values": rows}
+        value_range_body = {"values": data}
         service.spreadsheets().values().append(
             spreadsheetId=spreadsheet_id,
             range=sheet_name,
@@ -106,4 +194,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     creds = auth(args.credentials)
-    upload(creds, args.person, args.filename, args.spreadsheet_id, args.sheet_name)
+    existing_data = read_existing_data(creds, args.spreadsheet_id, args.sheet_name)
+    data_to_upload = read_data_to_upload(args.person, args.filename)
+    deduped = deduplicate(existing_data, data_to_upload)
+    if not deduped:
+        print("Entire input file has already been uploaded")
+        exit(0)
+    upload(creds, args.spreadsheet_id, args.sheet_name, deduped)
